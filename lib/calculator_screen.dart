e4481a53ddb8e5f8908fb8fb88359f68e9d69e8e
evopimp <31068621+evopimp@users.noreply.github.com>
2023-10-25 20:45:32 -0500

    Itial flutter app upload
    This is the first upload of content of the app, Including a calculator interface and indapendent fonts.typeface, and colors in their own .dart files

---
+++ lib/calculator_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CalculatorScreen extends StatefulWidget {
+  @override
+  _CalculatorScreenState createState() => _CalculatorScreenState();
+}
+
+class _CalculatorScreenState extends State<CalculatorScreen> {
+  // ... Your calculator screen code here
+}
